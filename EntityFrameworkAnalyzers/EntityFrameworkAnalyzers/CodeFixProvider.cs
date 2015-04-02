84cdab2d60d682e1d010ecf3cea80eed3d2987e0
Giorgi Dalakishvili <giorgi@aboutmycode.com>
2015-04-02 14:00:17 +0400

    Find available variable name for lambda parameter name.

--- EntityFrameworkAnalyzers/EntityFrameworkAnalyzers/CodeFixProvider.cs
+++ EntityFrameworkAnalyzers/EntityFrameworkAnalyzers/CodeFixProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
 using System.Threading;
@@ -43,10 +44,12 @@ namespace EntityFrameworkAnalyzers
 
 		private async Task<Document> LiteralToLambdaAsync(Document document, InvocationExpressionSyntax invocationExpr, CancellationToken cancellationToken)
 		{
+			var lambdaVariableName = await FindAvailabeVariableName(document, invocationExpr, cancellationToken);
+
 			var argumentList = invocationExpr.ArgumentList;
 			var incudePath = argumentList.Arguments[0].Expression;
 
-			var lambdaPath = string.Format("a => a.{0}", incudePath.ToFullString().Trim('"'));
+			var lambdaPath = string.Format("{0} => {0}.{1}", lambdaVariableName, incudePath.ToFullString().Trim('"'));
 
 			var lambdaExpression = SyntaxFactory.ParseExpression(lambdaPath)
 												.WithAdditionalAnnotations(Formatter.Annotation);
@@ -66,6 +69,45 @@ namespace EntityFrameworkAnalyzers
 
 			var newDocument = document.WithSyntaxRoot(newRoot);
 			return newDocument;
+		}
+
+		private static async Task<string> FindAvailabeVariableName(Document document, SyntaxNode invocationExpr, CancellationToken cancellationToken)
+		{
+			var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+
+			int currentChar = 1;
+			do
+			{
+				var name = ToVariableName(currentChar);
+				var immutableArray = semanticModel.LookupSymbols(invocationExpr.SpanStart, name: name);
+
+				if (immutableArray.Length == 0)
+				{
+					return name;
+				}
+
+				if (immutableArray.All(symbol => symbol.Kind != SymbolKind.Local && symbol.Kind != SymbolKind.Parameter))
+				{
+					return name;
+				}
+
+				currentChar++;
+			} while (true);
+		}
+
+		private static string ToVariableName(int number)
+		{
+			var dividend = number;
+			var result = string.Empty;
+
+			while (dividend > 0)
+			{
+				var modulo = (dividend - 1) % 26;
+				result = Convert.ToChar('a' + modulo) + result;
+				dividend = (dividend - modulo) / 26;
+			}
+
+			return result;
 		}
 	}
 }
