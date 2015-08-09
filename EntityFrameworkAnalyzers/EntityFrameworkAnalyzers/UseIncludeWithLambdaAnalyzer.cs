f36f5c4377dbf964486a0f09cb832a0eecc7ebe6
Giorgi Dalakishvili <mcnamaragio@gmail.com>
2015-08-09 16:45:32 +0400

    Changed analyzer category.

--- EntityFrameworkAnalyzers/EntityFrameworkAnalyzers/UseIncludeWithLambdaAnalyzer.cs
+++ EntityFrameworkAnalyzers/EntityFrameworkAnalyzers/UseIncludeWithLambdaAnalyzer.cs
@@ -19,7 +19,7 @@ namespace EntityFrameworkAnalyzers
 		internal static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
 		internal static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
 		internal static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-		internal const string Category = "Naming";
+		internal const string Category = "Usage";
 
 		internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true);
 
